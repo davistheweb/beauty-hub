efd8cfc4c60d98cc3ff8c701d0f3e65d62c4dfd6
davistheweb <josiahdave001@gmail.com>
2025-09-22 14:08:14 +0100

    auth

--- src/app/auth/account-recovery/page.tsx
+++ src/app/auth/account-recovery/page.tsx
@@ -1,14 +1,6 @@
 "use client";
-import { AuthWrapper } from "../_components/AuthWrapper";
 import ForgotPasswordForm from "../_components/ForgotPasswordForm";
 
 export default function LoginPage() {
-  return (
-    <AuthWrapper
-      formTitle="Forgot Password"
-      formSubtitle="Don't worry, We'll send you reset instructions"
-    >
-      <ForgotPasswordForm />
-    </AuthWrapper>
-  );
+  return <ForgotPasswordForm />;
 }
--- src/app/auth/otp/page.tsx
+++ src/app/auth/otp/page.tsx
@@ -1,14 +1,6 @@
 "use client";
-import { AuthWrapper } from "../_components/AuthWrapper";
 import { OtpForm } from "../_components/OtpForm";
 
 export default function OTP() {
-  return (
-    <AuthWrapper
-      formTitle="Enter OTP"
-      formSubtitle="A 4 digits code has been sent to your email <span style='color: #1AB65C;'>*****oe@gmail.com</span> to verify account."
-    >
-      <OtpForm />
-    </AuthWrapper>
-  );
+  return <OtpForm />;
 }
