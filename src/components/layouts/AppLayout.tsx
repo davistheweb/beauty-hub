799a40fb316d9be139f0fe45cc435a5ccf4426de
davistheweb <josiahdave001@gmail.com>
2025-09-24 22:13:41 +0100

    App Layout stye update

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -33,17 +33,19 @@ function AppLayout({ children }: { children: React.ReactNode }) {
   //   }
 
   return (
-    <div className="relative flex h-full min-h-svh w-full bg-white">
-      <Toaster
-        richColors
-        position="top-right"
-      />
-      <Sidebar />
-      <div className="relative flex h-full w-full flex-col overflow-x-hidden pt-4">
-        <Navbar />
-        <main className="scrollbar-thin h-screen w-full flex-1 overflow-x-hidden overflow-y-auto bg-[#E6E6E6] p-4 pt-[80px] lg:py-5">
-          {children}
-        </main>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="relative flex h-full min-h-svh w-full max-w-7xl bg-white">
+        <Toaster
+          richColors
+          position="top-right"
+        />
+        <Sidebar />
+        <div className="relative flex h-full w-full flex-col overflow-x-hidden pt-4">
+          <Navbar />
+          <main className="scrollbar-thin h-screen w-full flex-1 overflow-x-hidden overflow-y-auto bg-[#E6E6E6] p-4 pt-[80px] lg:py-5">
+            {children}
+          </main>
+        </div>
       </div>
     </div>
   );
