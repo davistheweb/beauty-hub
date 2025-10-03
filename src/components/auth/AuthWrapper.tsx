590240ec788b5e5426727078ede7f26b643fdc08
davistheweb <josiahdave001@gmail.com>
2025-10-03 11:45:00 +0100

    styles fix

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -45,14 +45,14 @@ export const AuthWrapper: React.FC<{
           />
         </div>
       </aside>
-      <main className="-mt-20 flex items-center justify-center md:mt-5 lg:items-start lg:justify-start">
+      <main className="-mt-20 flex items-center justify-center md:mt-5 lg:items-start lg:justify-start h-full">
         <AnimatePresence>
           <motion.div
             initial={{ x: "100vw" }}
             animate={{ x: 0 }}
             exit={{ x: "100vw" }}
             transition={{ type: "tween", duration: 0.8 }}
-            className="w-full max-sm:max-w-md"
+            className="w-full max-sm:max-w-md flex items-center justify-center"
           >
             <Toaster
               richColors
