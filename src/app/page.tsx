b1fe51c5245248239350c962859f3f805aa7e944
davistheweb <josiahdave001@gmail.com>
2025-10-13 10:42:04 +0100

    :

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Redirect from "@/Redirect";
-import Dashboard from "./dashboard/page";
 
 export default function page() {
   return (
     <>
       <Redirect url="dashboard" />
-      <Dashboard />
     </>
   );
 }
