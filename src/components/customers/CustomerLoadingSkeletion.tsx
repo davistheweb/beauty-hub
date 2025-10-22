1df1c0e59be625e83446ea606f299061d237d730
davistheweb <josiahdave001@gmail.com>
2025-10-22 10:07:20 +0100

    customers styles fix

--- src/components/customers/CustomerLoadingSkeletion.tsx
+++ src/components/customers/CustomerLoadingSkeletion.tsx
@@ -8,7 +8,10 @@ export default function CustomerLoadingSkeletion() {
       <Skeleton className="h-[10px] w-[200px] bg-[#E6E6E6]" />
 
       <div className="grid place-items-center gap-2 sm:grid-cols-2">
-        <CardSkeleton className="h-[200px] w-full md:w-[490px]" />
+        <CardSkeleton
+          className="sm:w[320px] h-[200px] w-full lg:w-[370px] xl:w-[490px]"
+          length={4}
+        />
       </div>
     </div>
   );
